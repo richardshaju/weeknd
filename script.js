70585b44416a87e6eb71ece3ef633d4a3c0e469d
Richard Shaju <richardshaju66@gmail.com>
2022-10-23 20:34:46 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,42 +1,101 @@
-let track_img = document.querySelector('.total-duration');
+let now_playing = document.querySelector('.now-playing');
+let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
-let curr_time = document.getElementsByClassName('curr_time')
-let total_duration = document.getElementsByClassName('ttotal_duration')
 
-let seek_slider = document.getElementsByClassName('.seek_slider');
-let volume_slider = document.getElementsByClassName('.volume_slider');
-document.getElementById('title').innerHTML = "Starboy"
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
+
+let seek_slider = document.querySelector('.seek_slider');
+let volume_slider = document.querySelector('.volume_slider');
+
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+
 
 let curr_track = document.createElement('audio');
 
-//document.getElementById('music').type = "audio/mpeg"
-//document.getElementById('music').src = ""
-//curr_track.setAttribute("src","./music_stay.mp3")
-curr_track.src = "./music_stay.mp3";
-curr_track.id = 'myAudio'
-curr_track.load();
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
 
-function nextTrack() {
-    setUpdate()
-    x = curr_track.duration
-    y = Math.floor(x);
+const music_list = [
+    {
+        img : 'https://m.media-amazon.com/images/I/51UVW34rJTL._SY355_.jpg',
+        name : 'I Was Never There',
+        music : 'music/i_was_never_there.mp3'
+    },
+  
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    reset();
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+    track_art.src = music_list[track_index].img 
+    track_name.textContent = music_list[track_index].name;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+}
+
+
+function reset(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
+}
+
+function playpauseTrack(){
+    isPlaying ? pauseTrack() : playTrack();
+}
+function playTrack(){
     curr_track.play();
-    document.getElementById('hi').innerHTML = '00:'+y;
-    console.log(y);
+    isPlaying = true;
+    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
-
-function setVolume() {
-    curr_track.volume = volume_slider.value / 100;
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
-
-function seekTo() {
+function nextTrack(){
+    if(track_index < music_list.length - 1 && isRandom === false){
+        track_index += 1;
+    }else if(track_index < music_list.length - 1 && isRandom === true){
+        let random_index = Number.parseInt(Math.random() * music_list.length);
+        track_index = random_index;
+    }else{
+        track_index = 0;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function prevTrack(){
+    if(track_index > 0){
+        track_index -= 1;
+    }else{
+        track_index = music_list.length -1;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function seekTo(){
     let seekto = curr_track.duration * (seek_slider.value / 100);
     curr_track.currentTime = seekto;
 }
-function setUpdate() {
+function setVolume(){
+    curr_track.volume = volume_slider.value / 100;
+}
+function setUpdate(){
     let seekPosition = 0;
-    if (!isNaN(curr_track.duration)) {
+    if(!isNaN(curr_track.duration)){
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
         seek_slider.value = seekPosition;
 
@@ -45,10 +104,10 @@ function setUpdate() {
         let durationMinutes = Math.floor(curr_track.duration / 60);
         let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
+        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
+        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
         total_duration.textContent = durationMinutes + ":" + durationMinutes;
