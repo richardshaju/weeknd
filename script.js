d420d10892c01c2a029d4305c47eccf98ffc1064
Richard Shaju <richardshaju66@gmail.com>
2022-10-23 20:43:55 +0530

    music added

--- script.js
+++ script.js
@@ -23,21 +23,48 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'https://m.media-amazon.com/images/I/51UVW34rJTL._SY355_.jpg',
-        name : 'I Was Never There',
-        music : 'music/i_was_never_there.mp3'
+        img: './images/starboy.jpg',
+        name: 'Starboy',
+        music: 'music/starboy.mp3'
     },
+    {
+        img: './images/blinding_lights.jpg',
+        name: 'Blinding Lights',
+        music: 'music/blinding_lights.mp3'
+    },
+    {
+        img: './images/i_was_never_there.jpg',
+        name: 'I Was Never There',
+        music: 'music/i_was_never_there.mp3'
+    },
+    {
+        img: './images/save_your_tears.jpg',
+        name: 'Save Your Tears',
+        music: 'music/save_your_tears.mp3'
+    },
+    {
+        img: './images/call_out.jpg',
+        name: 'Call Out My Name',
+        music: 'music/call_out.mp3'
+    },
+    {
+        img: './images/heartless.jpg',
+        name: 'Heartless',
+        music: 'music/heartless.mp3'
+    },
+
   
+
 ];
 
 loadTrack(track_index);
 
-function loadTrack(track_index){
+function loadTrack(track_index) {
     clearInterval(updateTimer);
     reset();
     curr_track.src = music_list[track_index].music;
     curr_track.load();
-    track_art.src = music_list[track_index].img 
+    track_art.src = music_list[track_index].img
     track_name.textContent = music_list[track_index].name;
 
     updateTimer = setInterval(setUpdate, 1000);
@@ -46,56 +73,56 @@ function loadTrack(track_index){
 }
 
 
-function reset(){
+function reset() {
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
 }
 
-function playpauseTrack(){
+function playpauseTrack() {
     isPlaying ? pauseTrack() : playTrack();
 }
-function playTrack(){
+function playTrack() {
     curr_track.play();
     isPlaying = true;
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
-function pauseTrack(){
+function pauseTrack() {
     curr_track.pause();
     isPlaying = false;
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
-function nextTrack(){
-    if(track_index < music_list.length - 1 && isRandom === false){
+function nextTrack() {
+    if (track_index < music_list.length - 1 && isRandom === false) {
         track_index += 1;
-    }else if(track_index < music_list.length - 1 && isRandom === true){
+    } else if (track_index < music_list.length - 1 && isRandom === true) {
         let random_index = Number.parseInt(Math.random() * music_list.length);
         track_index = random_index;
-    }else{
+    } else {
         track_index = 0;
     }
     loadTrack(track_index);
     playTrack();
 }
-function prevTrack(){
-    if(track_index > 0){
+function prevTrack() {
+    if (track_index > 0) {
         track_index -= 1;
-    }else{
-        track_index = music_list.length -1;
+    } else {
+        track_index = music_list.length - 1;
     }
     loadTrack(track_index);
     playTrack();
 }
-function seekTo(){
+function seekTo() {
     let seekto = curr_track.duration * (seek_slider.value / 100);
     curr_track.currentTime = seekto;
 }
-function setVolume(){
+function setVolume() {
     curr_track.volume = volume_slider.value / 100;
 }
-function setUpdate(){
+function setUpdate() {
     let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
+    if (!isNaN(curr_track.duration)) {
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
         seek_slider.value = seekPosition;
 
@@ -104,10 +131,10 @@ function setUpdate(){
         let durationMinutes = Math.floor(curr_track.duration / 60);
         let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
-        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
-        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
         total_duration.textContent = durationMinutes + ":" + durationMinutes;
